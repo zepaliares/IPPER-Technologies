c7bddc5377f291c36f11a34ea68fc60ef98a9ebb
zepaliares <134644593+zepaliares@users.noreply.github.com>
2023-05-31 19:43:46 -0300

    Add files via upload

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -4,54 +4,11 @@ export const metadata = {
 }
 
 import Link from 'next/link'
-import React from 'react';
 import connectToDatabase from '../../../conexao';
-
 
 export default function SignIn() {
 
-  class Login extends React.Component<{}, any> {
-    constructor(props: {}) {
-      super(props);
-  
-      this.state = {
-        email: '',
-        password: '',
-        errorMessage: '',
-      };
-    }
-  
-    handleLogin = async () => {
-      const { email, password } = this.state;
-  
-      try {
-        const db = await connectToDatabase();
-        const collection = db.collection('dados');
-        const user = await collection.findOne({ email });
-  
-        if (user && user.password === password) {
-          // Login successful
-          this.setState({ errorMessage: 'Logged in successfully!' });
-        } else {
-          // Invalid email or password
-          this.setState({ errorMessage: 'Invalid email or password' });
-        }
-      } catch (error) {
-        // Error connecting to the database
-        this.setState({ errorMessage: 'Error connecting to the database' });
-        console.error(error);
-      }
-    };
-  
-     handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const { name, value } = event.target;
-      this.setState({ [name]: value });
-    };
-  
-    render() {
-      const { email, password, errorMessage } = this.state;
-
-  
+  const erroLogin = "Credenciais erradas"
   return (
     <section className="bg-preto-fundo"style={{ minHeight: '100vh' }}>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
@@ -72,17 +29,17 @@ export default function SignIn() {
             <form>
               <div className="flex flex-wrap -mx-3 mb-4">
                 <div className="w-full px-3">
-                  <label className="block text-gray-400 text-sm font-medium mb-1" htmlFor="email" >Email</label>
-                  <input id="email" type="email" className="form-input w-full text-gray-800" placeholder="Insira seu endereço de email" required value={email} onChange={this.handleChange}/>
+                  <label className="block text-gray-400 text-sm font-medium mb-1" htmlFor="email">Email</label>
+                  <input id="email" type="email" className="form-input w-full text-gray-800" placeholder="Insira seu endereço de email" required />
                 </div>
               </div>
               <div className="flex flex-wrap -mx-3 mb-4">
                 <div className="w-full px-3">
                   <div className="flex justify-between">
-                    <label className="block text-gray-400 text-sm font-medium mb-1" htmlFor="password" >Senha</label>
+                    <label className="block text-gray-400 text-sm font-medium mb-1" htmlFor="password">Senha</label>
                    
                   </div>
-                  <input id="password" type="password" className="form-input w-full text-gray-800" placeholder="Insira sua senha" required value={password} onChange={this.handleChange}/>
+                  <input id="password" type="password" className="form-input w-full text-gray-800" placeholder="Insira sua senha" required />
                   
                 </div>
                 
@@ -103,10 +60,11 @@ export default function SignIn() {
               </div>
               <div className="flex flex-wrap -mx-3 mt-6">
                 <div className="w-full px-3">
-                  <button className="btn text-white bg-indigo-600 hover:bg-indigo-700 w-full" onClick={this.handleLogin}>Entrar</button>
+                  <button className="btn text-white bg-indigo-600 hover:bg-indigo-700 w-full">Entrar</button>
                 </div>
-                <div>{errorMessage}</div>
               </div>
+
+              {erroLogin && <p>{erroLogin}</p>}
             </form>
            
             
@@ -117,4 +75,3 @@ export default function SignIn() {
     </section>
   )
 }
-  }}
